13542a6ecdb1f7a2db6b24f749124b570ba5fffa
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-08-01 21:33:25 +0200

    Created mix project

---
+++ lib/graft.ex
@@ -0,0 +1,18 @@
+defmodule Graft do
+  @moduledoc """
+  Documentation for Graft.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Graft.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/graft_test.exs
@@ -0,0 +1,8 @@
+defmodule GraftTest do
+  use ExUnit.Case
+  doctest Graft
+
+  test "greets the world" do
+    assert Graft.hello() == :world
+  end
+end
