3ff0ce228409e75f666d0ab7ca4715fd8961bfc2
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-08-03 16:12:26 +0200

    Creating general state with generated time_out and registering it.

--- lib/server.ex
+++ lib/server.ex
@@ -1,17 +1,23 @@
 defmodule Graft.Server do
     def start do
-        spawn fn -> follower() end
+        spawn fn -> init() end
+    end
+
+    def init() do
+        Process.register(Graft.State.create_state(:general, generate_time_out(), general_state())
+        follower()
     end
 
     def follower do
         receive do
-            {:msg, msg} -> IO.puts(msg)
+            {:msg, msg} ->
+                IO.puts(msg)
+                follower()
         after
-            10_000 ->
-                IO.puts("Timed out, beginning election") 
+            ms = time_out() ->
+                IO.puts("Timed out #{ms}, beginning election")
                 candidate()
         end
-        follower()
     end
 
     def candidate do
@@ -20,5 +26,17 @@ defmodule Graft.Server do
 
     def leader do
 
+    end
+
+    defp generate_time_out do: :rand.uniform(50)*100+5000)
+
+    defp time_out do
+        send general_state(), {:get, :time_out, self()}
+        receive do ms -> ms end
+    end
+
+    defp general_state do
+        pid_string = String.replace((inspect self()), ["#",".","<",">"], "_")
+        String.to_atom("general_state" <> pid_string)
     end
 end
