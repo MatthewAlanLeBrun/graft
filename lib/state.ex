45590c9f3b3e8a0338aadac2c5408742a01651a0
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2019-09-14 12:37:54 +0200

    Merging structs into one.

--- lib/state.ex
+++ lib/state.ex
@@ -1,57 +1,14 @@
-defmodule Graft.State.Server.General do
-    defstruct time_out: 0,          # random time out of server
-              state: :follower      # current state of the server
-end
-
-defmodule Graft.State.Server.Persistent do
-    defstruct current_term: 0,      # latest term server has seen
-              voted_for: nil,       # candidate_pid that received vote in current term (or null if none)
-              log: []               # log entries; each entry contains command for state machine, and term when entry was received by leader
-end
-
-defmodule Graft.State.Server.Volatile do
-    defstruct commit_index: 0,      # index of highest log entry known to be committed
-              last_applied: 0       # index of highest log entry applied to state machine
-end
-
-defmodule Graft.State.Leader.Volatile do
-    defstruct next_index: [],       # for each server, index of the next log entry to send to that server
-              match_index: []       # for each server, index of highest log entry known to be replicated on server
-end
-
-defmodule Graft.State.Cluster do
-    defstruct server_count: 0,      # number of servers in the cluster
-              server_pids: []       # pids of each server in the cluster
-end
-
 defmodule Graft.State do
-    defstruct general:    %Graft.State.Server.General{},
-              persistent: %Graft.State.Server.Persistent{},
-              volatile:   %Graft.State.Server.Volatile{},
-              leader:     nil
-
-    def create_state do
-        spawn_link(fn() -> state(%Graft.State{}) end);
-    end
-
-    def update(struct, key, value) do
-        send Graft.StateFactory.get_state(), {:put, struct, key, value}
-    end
-
-    def get(struct, key) do
-        send Graft.StateFactory.get_state(), {:get, struct, key, self()}
-        receive do
-            {:ok, data} -> data
-        end
-    end
-
-    defp state(%Graft.State{} = state) do
-        receive do
-            {:get, within, key, from} ->
-                send from, {:ok, Map.get(state, within) |> Map.get(key)}
-                state(state)
-            {:put, into, key, value} ->
-                Map.put(state, into, Map.get(state, into) |> Map.put(key, value)) |> state()
-        end
-    end
+    defstruct time_out: 0,          # random time out of server
+              state: :follower,     # current state of the server
+              me: nil,              # name of the server process
+              current_term: 0,      # latest term server has seen
+              voted_for: nil,       # candidate_pid that received vote in current term (or null if none)
+              log: [{0, 0, nil}],   # log entries; each entry contains command for state machine, and term when entry was received by leader
+              commit_index: 0,      # index of highest log entry known to be committed
+              last_applied: 0,      # index of highest log entry applied to state machine
+              next_index: [],       # for each server, index of the next log entry to send to that server
+              match_index: [],      # for each server, index of highest log entry known to be replicated on server
+              server_count: 0,      # number of servers in the cluster
+              servers: []           # pids of each server in the cluster
 end
