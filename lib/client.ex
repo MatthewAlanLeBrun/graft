2d70e4b60e0f59307ffec35f68ec62c66cedf7e7
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-04-01 18:32:19 +0200

    Adding read and write operations available to the client.

--- lib/client.ex
+++ lib/client.ex
@@ -1,5 +1,15 @@
 defmodule Graft.Client do
-    def send_job(server, job) do
-        send server, job
+    def read(server, key) do
+        case GenStateMachine.call(server, {:entry, :read, key}) do
+            {:ok, value} -> {:ok, value}
+            {:error, {:redirect, leader}} -> read(leader, key)
+        end
+    end
+
+    def write(server, {key, value}) do
+        case GenStateMachine.call(server, {:entry, :write, {key, value}}}) do
+            :ok -> :ok
+            {:error, {:redirect, leader}} -> write(leader, {key, value})
+        end
     end
 end
