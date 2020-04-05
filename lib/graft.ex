9ea5c8d1abd9f22b2d074b5b6d73f703b6f712f8
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-04-05 18:16:18 +0200

    Proper indentation and adding a function to start 3 servers

--- lib/graft.ex
+++ lib/graft.ex
@@ -1,21 +1,22 @@
 defmodule Graft do
-  @moduledoc """
-  Documentation for Graft.
-  """
+    @moduledoc """
+    Documentation for Graft.
+    """
 
-  def start(servers) do
-      for server <- servers, do: Graft.Server.start(server, servers)
-      for server <- servers, do: GenStateMachine.cast(server, :start)
-  end
+    def start(servers) do
+        for server <- servers, do: Graft.Server.start(server, servers)
+        for server <- servers, do: GenStateMachine.cast(server, :start)
+    end
 
-  def start5, do: start([:server1, :server2, :server3, :server4, :server5])
+    def start5, do: start([:server1, :server2, :server3, :server4, :server5])
+    def start3, do: start([:server1, :server2, :server3])
 
-  def data(server), do: GenStateMachine.call(server, :data)
+    def data(server), do: GenStateMachine.cast(server, :data)
 
-  def all_data(servers) do
-      for server <- servers do
-          GenStateMachine.call(server, :data)
-      end
-  end
+    def all_data(servers) do
+        for server <- servers do
+            GenStateMachine.call(server, :data)
+        end
+    end
 
 end
