db47050da4d35b7a914ff2b07670a96f3a6f61ce
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-08-08 11:51:50 +0200

    A wild stack machine appeared.

---
+++ machines/stack_machine.ex
@@ -0,0 +1,25 @@
+defmodule MyStackMachine do
+    use Graft.Machine
+
+    @impl Graft.Machine
+    def init([]) do
+        {:ok, []}
+    end
+
+    @impl Graft.Machine
+    def handle_entry({:put, value}, state) do
+        {:ok, [value | state]}
+    end
+
+    def handle_entry(:pop, []) do
+        {:noop, []}
+    end
+
+    def handle_entry(:pop, [response | state]) do
+        {response, state}
+    end
+
+    def handle_entry(_, state) do
+        {:invalid_request, state}
+    end
+end
