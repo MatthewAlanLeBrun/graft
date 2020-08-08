95e4939019e395afbad04b433b89fea97aee9493
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-08-08 11:24:02 +0200

    Added logger config.

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,9 @@
 import Config
 
 config :graft,
-    cluster: [{:server1, :server1@localhost}, {:server2, :server1@localhost}, {:server3, :server1@localhost}],
+    cluster: [{:server1, :me@localhost}, {:server2, :me@localhost}, {:server3, :me@localhost}],
     machine: MyStackMachine,
     machine_args: []
+
+config :logger,
+    level: :info
