afe051831c150ea23c583186fd991b0f97077c47
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-08-10 19:50:01 +0200

    Allows config for monitor. Also green logs.

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,12 @@
 import Config
 
 config :graft,
-    cluster: [{:server1, :me@localhost}, {:server2, :me@localhost}, {:server3, :me@localhost}],
+    cluster: [{:server1, :node0@localhost}, {:server2, :node1@localhost}, {:server3, :node1@localhost}],
     machine: MyStackMachine,
-    machine_args: []
+    machine_args: [],
+    monitor: [module: Graft.Supervisor, function: :start_link, args: [], hml: &:bypass.mfa_spec/1]
+    # monitor: :off
 
-config :logger,
+config :logger, :console,
+    colors: [info: :green],
     level: :info
