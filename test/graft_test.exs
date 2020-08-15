f64e76e4aaa7af0de2bfd3d4a9484e6a26f17c93
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-08-15 19:03:21 +0200

    Forcing a leader check.

--- test/graft_test.exs
+++ test/graft_test.exs
@@ -1,107 +1,29 @@
 defmodule GraftTest do
     use ExUnit.Case
-
-    # AE refers to an append entries rpc
-    # RV refers to a request votes rpc
-    # 'up-to-date' has the same meaning as defined in https://raft.github.io/raft.pdf
-
-    test "follower receives AE with outdated term" do
-
+    
+    setup_all do
+        cluster = Application.fetch_env! :graft, :cluster
+        %{cluster: cluster}
     end
 
-    test "follower receives AE with a far ahead log" do
-
+    setup %{cluster: cluster} do
+        for {server, _} <- cluster do
+            Graft.restart_server server
+        end
+        
+        on_exit fn ->
+            for {server, _} <- cluster do
+                Graft.restart_server server
+            end
+        end
+        
+        :ok
     end
 
-    test "follower with matching log receives AE" do
-
-    end
-
-    test "follower with mismatching log receives AE" do
-
-    end
-
-    test "follower receives RV with outdated term" do
-
-    end
-
-    test "follower receives RV from candidate with a not as up-to-date log as the follower's" do
-
-    end
-
-    test "follower that already voted receives a RV rpc from a different candidate" do
-
-    end
-
-    test "follower that already voted receives a RV rpc from the same candidate" do
-
-    end
-
-    test "follower receives a valid RV rpc" do
-
-    end
-
-    test "candidate receives rpc with term > current term" do
-
-    end
-
-    test "leader receives rpc with term > current term" do
-
-    end
-
-    test "follower converts to candidate on timeout" do
-
-    end
-
-    test "servers just converted to candidates start election" do
-
-    end
-
-    test "candidate receives majority votes" do
-
-    end
-
-    test "candidate receives AE from new leader" do
-
-    end
-
-    test "candidate receives AE from old leader" do
-
-    end
-
-    test "candidate restarts new election on timeout" do
-
-    end
-
-    test "leader sends heartbeat on election" do
-
-    end
-
-    test "leader appends entry to local log when receiving client request" do
-
-    end
-
-    test "leader increments next index map for a server on a successful AE reply" do
-
-    end
-
-    test "leader decrements next index map for a server on a unsuccessful AE reply" do
-
-    end
-
-    test "leader updates commit index when majority of servers have a matching log" do
-
-    end
-
-    test "follower with a commit index larger than last applied index will apply the next entry in its log" do
-
-    end
-
-    test "candidate with a commit index larger than last applied index will apply the next entry in its log" do
-
-    end
-
-    test "leader with a commit index larger than last applied index will apply the next entry in its log" do
-
+    test "forcing a leader" do
+        Graft.force_promotion :s1
+        assert {:s1, node()} == Graft.leader :s1
+        assert {:s1, node()} == Graft.leader :s2
+        assert {:s1, node()} == Graft.leader :s3
     end
 end
