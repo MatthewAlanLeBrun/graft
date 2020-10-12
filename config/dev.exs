da7f083f95518b56c32c4027137b86bfadb05629
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2020-10-12 11:29:48 +0200

    Chaging machine and monitor.

--- config/dev.exs
+++ config/dev.exs
@@ -2,11 +2,11 @@ import Config
 
 config :graft,
     cluster: [{:server1, :nonode@nohost}, {:server2, :nonode@nohost}, {:server3, :nonode@nohost}],
-    machine: MyStackMachine,
+    machine: MyKVMachine,
     machine_args: [],
-    # monitor: [module: Graft.Supervisor, function: :start_link, args: [], hml: &:election_safety.mfa_spec/1]
+    # monitor: [module: Graft.Supervisor, function: :start_link, args: [], hml: &:leader_completeness.mfa_spec/1]
     monitor: false
 
 config :logger, :console,
     colors: [info: :green],
-    level: :debug
+    level: :info
