1aafc2ee9309fb820c69ef96f4c4589fe41c9513
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2021-05-25 10:09:18 +0200

    Formatting.

--- lib/graft/client.ex
+++ lib/graft/client.ex
@@ -1,10 +1,10 @@
 defmodule Graft.Client do
-    @moduledoc false
-    def request(server, entry) do
-        case GenStateMachine.call(server, {:entry, entry}) do
-            {:ok, response} -> response
-            {:error, {:redirect, leader}} -> request(leader, entry)
-            {:error, msg} -> msg
-        end
+  @moduledoc false
+  def request(server, entry) do
+    case GenStateMachine.call(server, {:entry, entry}) do
+      {:ok, response} -> response
+      {:error, {:redirect, leader}} -> request(leader, entry)
+      {:error, msg} -> msg
     end
+  end
 end
