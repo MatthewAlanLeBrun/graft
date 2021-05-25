1338b37d283da11787e1f30e4e45e01062921314
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2021-05-25 09:48:55 +0200

    Cleaning up.

--- machines/sum_machine.ex
+++ machines/sum_machine.ex
@@ -1,10 +1,10 @@
 defmodule MySumMachine do
-    use Graft.Machine
-  
-    @impl Graft.Machine
-    def init([]), do: {:ok, 0}
-  
-    @impl Graft.Machine
-    def handle_entry(x, acc) when is_number(x), do: {x+acc, x+acc}
-    def handle_entry(_, acc), do: {:invalid_request, acc}
+  use Graft.Machine
+
+  @impl Graft.Machine
+  def init([]), do: {:ok, 0}
+
+  @impl Graft.Machine
+  def handle_entry(x, acc) when is_number(x), do: {x+acc, x+acc}
+  def handle_entry(_, acc), do: {:invalid_request, acc}
 end
