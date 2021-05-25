f27d4298fd1e37bdf8345bc1d191b6acc74dfa7c
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2021-05-25 09:53:59 +0200

    Forcing promotion removed.

--- test/graft_test.exs
+++ test/graft_test.exs
@@ -1,29 +1,3 @@
 defmodule GraftTest do
-    use ExUnit.Case
-    
-    setup_all do
-        cluster = Application.fetch_env! :graft, :cluster
-        %{cluster: cluster}
-    end
-
-    setup %{cluster: cluster} do
-        for {server, _} <- cluster do
-            Graft.restart_server server
-        end
-        
-        on_exit fn ->
-            for {server, _} <- cluster do
-                Graft.restart_server server
-            end
-        end
-        
-        :ok
-    end
-
-    test "forcing a leader" do
-        Graft.force_promotion :s1
-        assert {:s1, node()} == Graft.leader :s1
-        assert {:s1, node()} == Graft.leader :s2
-        assert {:s1, node()} == Graft.leader :s3
-    end
+  use ExUnit.Case
 end
