2498fcde8f31771d4b622eee0053c99836fb1b98
Matthew Alan Le Brun <matthew-alan.lebrun.17@um.edu.mt>
2021-05-25 09:37:44 +0200

    Adding ExCoveralls.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule Graft.MixProject do
       elixir: "~> 1.9",
       elixirc_paths: ["lib", "machines"],
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -16,15 +23,15 @@ defmodule Graft.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Graft, []}
+      mod: {Graft, []},
+      registered: [Graft.Supervisor]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.10", only: :test},
       {:gen_state_machine, "~> 2.0"}
     ]
   end
